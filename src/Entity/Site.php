6ded169ad8f87d2a84a335ab9fbd30b295852582
fonlupt.lucile@gmail.comf <fonlupt.lucile@gmail.com>
2025-03-25 11:23:11 +0100

    Creation des entity Lieu/participant/site

---
+++ src/Entity/Site.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SiteRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SiteRepository::class)]
+class Site
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomSite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getIdSite(): ?int
+    {
+        return $this->idSite;
+    }
+
+    public function setIdSite(int $idSite): static
+    {
+        $this->idSite = $idSite;
+
+        return $this;
+    }
+
+    public function getNomSite(): ?string
+    {
+        return $this->nomSite;
+    }
+
+    public function setNomSite(string $nomSite): static
+    {
+        $this->nomSite = $nomSite;
+
+        return $this;
+    }
+}
