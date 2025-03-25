7322e85ce09b95439978c7cec9f3f2f53c7e6b8c
SedLexLam <alexandre.lelamer@gmail.com>
2025-03-25 16:31:35 +0100

    Formulaire sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\SortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -16,6 +20,47 @@ final class SortieController extends AbstractController
         return $this->render('sortie/liste.html.twig', [
             'sorties' => $sorties,
         ]);
+    }
+    #[Route('/sortie/create', name: 'sortie_create', methods: ['GET', 'POST'])]
+    public function create(
+        Request                $request,
+        EntityManagerInterface $em,
+        SortieRepository $sortieRepository
+
+    ): Response
+    {
+        $sorties = $sortieRepository->findAll();
+        //Création de l'entité vide
+        $sortie = new Sortie();
+        $sortie->setOrganisateur(null);
+
+        //Création du formulaire et association de l'entité vide.
+
+        $sortieForm = $this->createForm(SortieType::class, $sortie);
+        //Récupère les données du formulaire et on les injecte dans notre $wish.
+        $sortieForm->handleRequest($request);
+        //On vérifie si le formulaire a été soumis et que les données soumises sont valides.
+        if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+            //Hydrater les propriétés absentes du formulaire
+//            $wish->setIsPublished(true);
+            //Sauvegarde dans la Bdd
+            //ajout de la relation avec le user
+            //$sortie->setUser($this->getUser());
+
+            $em->persist($sortie);
+            $em->flush();
+
+            //Affiche un message à l'utilisateur sur la prochaine page.
+            $this->addFlash('success', 'Ta sortie a bien été créée!');
+
+            //Redirige vers la page de detail du wish
+           // return $this->redirectToRoute('sortie.html.twig');
+            return $this->render('sortie/liste.html.twig', [
+                'sorties' => $sorties,
+            ]);
+        }
+        //Affiche le formulaire
+        return $this->render('sortie/create.html.twig', ["sortieForm" => $sortieForm]);
     }
 
 }
---
+++ src/Form/SortieType.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Etat;
+use App\Entity\Lieu;
+use App\Entity\Participant;
+use App\Entity\Site;
+use App\Entity\Sortie;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nomSortie')
+            ->add('dateHeureDebut', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('duree')
+            ->add('dateLimiteInscription', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('etat', EntityType::class, [
+                'class' => Etat::class,
+                'choice_label' => 'id',
+                'disabled' => true
+            ])
+            ->add('participants', EntityType::class, [
+                'class' => Participant::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+            ->add('organisateur', EntityType::class, [
+                'class' => Participant::class,
+                'choice_label' => 'id',
+            ])
+            ->add('site', EntityType::class, [
+                'class' => Site::class,
+                'choice_label' => 'nomSite',
+            ])
+            ->add('lieu', EntityType::class, [
+                'class' => Lieu::class,
+                'choice_label' => 'id',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
