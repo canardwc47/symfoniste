701e590af3f6cb4086be78c84759931b01efdb0e
fonlupt.lucile@gmail.comf <fonlupt.lucile@gmail.com>
2025-03-26 12:16:39 +0100

    redirection page d'accueil

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -49,7 +49,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('sortie_create'));
+        return new RedirectResponse($this->urlGenerator->generate('sortie_liste'));
 
     }
 
