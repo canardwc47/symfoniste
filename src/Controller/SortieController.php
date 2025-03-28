7901e489030cba9442db1536635e3913a7ab0e58
SedLexLam <alexandre.lelamer@gmail.com>
2025-03-28 16:55:55 +0100

    Templates

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -28,7 +28,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class SortieController extends AbstractController
 {
-    #[Route('/sortie', name: 'sortie_liste', methods: ['GET'])]
+    #[Route('/', name: 'sortie_liste', methods: ['GET'])]
     public function liste(SortieRepository $sortieRepository): Response
     {
         $participant = $this->getUser(); // Récupère l'utilisateur connecté
