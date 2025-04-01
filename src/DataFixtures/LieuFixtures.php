22e379018deea21b70912de6b35bf6749068e5c8
SedLexLam <alexandre.lelamer@gmail.com>
2025-04-01 11:08:17 +0200

    Templates

--- src/DataFixtures/LieuFixtures.php
+++ src/DataFixtures/LieuFixtures.php
@@ -19,7 +19,7 @@ class LieuFixtures extends Fixture implements DependentFixtureInterface
 
         $prefix = ["Au", "The", "El", "My", "Du", "", "", "Das", "One", "Ein"];
         $adjectives = ["Nocturne", "Gemütlich","Rustikal", "Secret", "Abgefahren","Cosmique", "Rouge", "Noir", "Bleu", "Doré", "Fou", "Secret", "Sexy", "Famoso", "Caliente", "Dancing", "Special", "Chill", "Crazy"];
-        $nouns = ["Lounge", "Palace", "Spot", "Temple", "Fusion", "Vortex", "Dynamite", "Cocktail", "Bistrot", "Cave", "Piano", "Brasserie", "Canaille", "Loutre", "Bar", "Mondschein", "Versteck", "Gasthaus", "Rooftop", "Kantine"]; ];
+        $nouns = ["Lounge", "Palace", "Spot", "Temple", "Fusion", "Vortex", "Dynamite", "Cocktail", "Bistrot", "Cave", "Piano", "Brasserie", "Canaille", "Loutre", "Bar", "Mondschein", "Versteck", "Gasthaus", "Rooftop", "Kantine"];
 
 
 
